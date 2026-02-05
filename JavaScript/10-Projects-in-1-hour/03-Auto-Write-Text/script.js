7021bfe566dbf85835c5ffee0e84b7e0d5be2248
Kariman Ayman Abd El Latif <karimanaymankaram@gmail.com>
2026-02-05 03:26:18 +0200

    10 Mini JavaScript Projects

---
+++ JavaScript/10-Projects-in-1-hour/03-Auto-Write-Text/script.js
@@ -0,0 +1,8 @@
+const text = "Hello World! I'm a text!!";
+let index = 0;
+function writeText() {
+  document.body.innerHTML = text.slice(0, index);
+  index++;
+  if (index > text.length) index = 0;
+}
+setInterval(writeText, 100);
